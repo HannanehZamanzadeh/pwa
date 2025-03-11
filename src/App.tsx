bb43e38f0eeb4fdb1878aac078d836d2894779e2
Your Name <you@example.com>
2025-03-11 11:18:49 +0330

    updated logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.tsx'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import appLogo from "/favicon.svg";
+import PWABadge from "./PWABadge.tsx";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <h1>welcome to our site</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={appLogo} className="logo" alt="pwa logo" />
@@ -31,7 +32,7 @@ function App() {
       </p>
       <PWABadge />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
